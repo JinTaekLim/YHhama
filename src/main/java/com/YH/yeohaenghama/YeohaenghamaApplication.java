103867ceb0b36584580accac13c9eeafb2cce772
JinTaekLim <jintaek019@gmail.com>
2024-01-12 15:58:38 +0900

    초기 커밋

---
+++ src/main/java/com/YH/yeohaenghama/YeohaenghamaApplication.java
@@ -0,0 +1,13 @@
+package com.YH.yeohaenghama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeohaenghamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YeohaenghamaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/YH/yeohaenghama/YeohaenghamaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.YH.yeohaenghama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeohaenghamaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
