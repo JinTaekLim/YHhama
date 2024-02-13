207618089f3c9e3b075e972dee25ee1649c6e8fe
JinTaekLim <jintaek019@gmail.com>
2024-02-13 15:48:00 +0900

    관광지/맛집 API

---
+++ src/main/java/com/YH/yeohaenghama/domain/test/dto/OpenApiDto.java
@@ -0,0 +1,15 @@
+package com.YH.yeohaenghama.domain.test.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class OpenApiDto {
+    private String numOfRows;
+    private String page;
+    private String MobileOS;
+    private String keyword;
+    private String contentTypeId;
+
+}
