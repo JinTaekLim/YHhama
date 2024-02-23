be8bbde122ea9d57638bba7f1717caa107ce26f3
임진택 <jintaeklim@imjintaeg-ui-MacBookAir.local>
2024-02-24 00:29:58 +0900

    feature/KAN-56

--- src/main/java/com/YH/yeohaenghama/domain/itinerary/controller/ItineraryController.java
+++ src/main/java/com/YH/yeohaenghama/domain/itinerary/controller/ItineraryController.java
@@ -29,22 +29,37 @@ public class ItineraryController {
     @Operation(summary = "일정 생성")
     @PostMapping("/join/{accountId}") //
     public ResponseEntity<ItineraryJoinDTO.Response> saveItinerary(@RequestBody ItineraryJoinDTO.Request reqDTO, @PathVariable Long accountId) {
-        ItineraryJoinDTO.Response response = itineraryService.save(reqDTO, accountId);
-        return ResponseEntity.ok(response);
+        try {
+            ItineraryJoinDTO.Response response = itineraryService.save(reqDTO, accountId);
+            return ResponseEntity.ok(response);
+        }
+        catch (Exception e){
+            return null;
+        }
     }
 
     @Operation(summary = "일정의 장소 추가")
     @PostMapping("/{itineraryId}")
     public ResponseEntity<String> createPlaces(@RequestBody List<PlaceJoinDTO> placeDTOs, @PathVariable Long itineraryId) {
-        placeService.createPlaces(placeDTOs, itineraryId);
-        return ResponseEntity.status(HttpStatus.CREATED).body("[일정]장소 추가 성공");
+        try {
+            placeService.createPlaces(placeDTOs, itineraryId);
+            return ResponseEntity.status(HttpStatus.CREATED).body("[일정]장소 추가 성공");
+        }
+        catch (Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("[일정]장소 추가 실패 : " + e.getMessage());
+        }
     }
 
     @Operation(summary = "제작된 일정 확인")
     @GetMapping("/showItinerary/{itineraryId}")
     public ResponseEntity<Itinerary> showItinerary(@PathVariable Long itineraryId){
-        return ResponseEntity.ok(itineraryService.show(itineraryId));
-
+        try {
+            return ResponseEntity.ok(itineraryService.show(itineraryId));
+        }
+        catch (Exception e){
+            e.getMessage();
+            return null;
+        }
     }
 
 
