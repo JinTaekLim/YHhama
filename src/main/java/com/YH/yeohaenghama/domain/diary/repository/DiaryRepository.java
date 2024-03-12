c3f76c56814212fee06c9217f64b5d2d22e04509
jintaeklim <jintaek019@gmail.com>
2024-03-12 11:27:23 +0900

    [수정] 일정 삭제시, 해당 일정으로 작성한 일기 동시 삭제

--- src/main/java/com/YH/yeohaenghama/domain/diary/repository/DiaryRepository.java
+++ src/main/java/com/YH/yeohaenghama/domain/diary/repository/DiaryRepository.java
@@ -1,7 +1,12 @@
 package com.YH.yeohaenghama.domain.diary.repository;
 
 import com.YH.yeohaenghama.domain.diary.entity.Diary;
+import com.YH.yeohaenghama.domain.itinerary.repository.ItineraryRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface DiaryRepository extends JpaRepository<Diary,Long> {
+    Optional<Diary> findByItinerary(Long itineraryId);
 }
