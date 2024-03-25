d1ceb81ceff7b8ae47da038a2be4a449f7b8d85d
JinTaekLim <jintaek019@gmail.com>
2024-03-25 19:13:11 +0900

    "[수정] 신고 스웨거 설명 추가"

--- src/main/java/com/YH/yeohaenghama/domain/report/controller/ReportController.java
+++ src/main/java/com/YH/yeohaenghama/domain/report/controller/ReportController.java
@@ -4,6 +4,7 @@ package com.YH.yeohaenghama.domain.report.controller;
 import com.YH.yeohaenghama.common.apiResult.ApiResult;
 import com.YH.yeohaenghama.domain.report.dto.ReportDiaryDTO;
 import com.YH.yeohaenghama.domain.report.service.ReportService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +20,7 @@ import java.util.NoSuchElementException;
 public class ReportController {
     private final ReportService reportService;
 
+    @Operation(summary = "신고", description = "typeId : 1 ( 일기 ) , 2 ( 리뷰 ) <br/> 위 번호 중 신고할 페이지의 ID를 입력해주세요.")
     @PostMapping("/diary")
     public ApiResult diary(ReportDiaryDTO dto){
         try{
