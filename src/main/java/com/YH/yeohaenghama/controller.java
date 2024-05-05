4719bd832d7023153c57f9ebf79446495b6dad95
JinTaekLim <jintaek019@gmail.com>
2024-05-05 21:17:57 +0900

    [추가] 채팅 테스트

---
+++ src/main/java/com/YH/yeohaenghama/controller.java
@@ -0,0 +1,15 @@
+//package com.YH.yeohaenghama;
+//
+//// HomeController.java
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//public class controller {
+//
+//    @GetMapping("/chat/room")
+//    public String home() {
+//        return "home"; // home.html의 파일 이름
+//    }
+//
+//}
