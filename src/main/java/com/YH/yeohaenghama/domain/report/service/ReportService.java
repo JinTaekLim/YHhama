f0f476e3a845b4e46a673d93de7d6c1e0fa3c038
JinTaekLim <jintaek019@gmail.com>
2024-05-06 23:39:14 +0900

    [추가] 관리자 페이지 일기 조회

--- src/main/java/com/YH/yeohaenghama/domain/report/service/ReportService.java
+++ src/main/java/com/YH/yeohaenghama/domain/report/service/ReportService.java
@@ -8,6 +8,8 @@ import com.YH.yeohaenghama.domain.diary.entity.Comment;
 import com.YH.yeohaenghama.domain.diary.entity.Diary;
 import com.YH.yeohaenghama.domain.diary.repository.CommentRepository;
 import com.YH.yeohaenghama.domain.diary.repository.DiaryRepository;
+import com.YH.yeohaenghama.domain.itinerary.entity.Itinerary;
+import com.YH.yeohaenghama.domain.itinerary.repository.ItineraryRepository;
 import com.YH.yeohaenghama.domain.report.dto.*;
 import com.YH.yeohaenghama.domain.report.entity.ReportComment;
 import com.YH.yeohaenghama.domain.report.entity.ReportDiary;
@@ -33,6 +35,7 @@ public class ReportService {
     private final ReportReviewRepository reportReviewRepository;
     private final ReportCommentRepository reportCommentRepository;
     private final AccountRepository accountRepository;
+    private final ItineraryRepository itineraryRepository;
     private final DiaryRepository diaryRepository;
     private final ReviewRepository reviewRepository;
     private final CommentRepository commentRepository;
@@ -89,6 +92,19 @@ public class ReportService {
 
     }
 
+
+    public List<ReportDiaryAllDTO.Response> diaryShow(){
+        List<Diary> diaryList = diaryRepository.findAll();
+
+        List<ReportDiaryAllDTO.Response> response = new ArrayList<>();
+        for(Diary diary : diaryList){
+            Optional<Itinerary> itineraryOpt = itineraryRepository.findById(diary.getItinerary());
+            List<ReportDiary> diaries = reportDiaryRepository.findByDiaryId(diary.getId());
+            response.add(ReportDiaryAllDTO.Response.fromEntity(diary, itineraryOpt.get().getAccount() , diaries.size()));
+        }
+
+        return response;
+    }
 
 
     public List<ReportDiaryDTO.Request> diaryReportList(){
