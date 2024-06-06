9d6a519e09e4f46d33d0194f72282a86ac679375
JinTaekLim <jintaek019@gmail.com>
2024-06-06 20:47:11 +0900

    [수정] 일정 조회시 공유중인 유저 함께 반환

--- src/main/java/com/YH/yeohaenghama/domain/itinerary/dto/ItineraryShowDTO.java
+++ src/main/java/com/YH/yeohaenghama/domain/itinerary/dto/ItineraryShowDTO.java
@@ -3,6 +3,7 @@ package com.YH.yeohaenghama.domain.itinerary.dto;
 import com.YH.yeohaenghama.domain.account.dto.AccountShowDTO;
 import com.YH.yeohaenghama.domain.account.entity.Account;
 import com.YH.yeohaenghama.domain.itinerary.entity.Itinerary;
+import com.YH.yeohaenghama.domain.itinerary.entity.ItineraryJoinAccount;
 import com.YH.yeohaenghama.domain.itinerary.entity.Place;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
@@ -18,7 +19,8 @@ import java.util.Map;
 public class ItineraryShowDTO {
     @Schema(description = "일정 ID")
     private Long itineraryId;
-
+    @Schema(description = "함께하는 유저")
+    private List<AccountShowDTO.Response> sharedAccount;
     @Schema(description = "동반자(0:기타, 1:혼자, 2:친구와, 3:연인과, 4:배우자와,5:아이와,6:부모님과)")
     private List<String> type;
     @Schema(description = "일정 스타일(1:체험－엑티비티, 2:sns－핫플레이스, 3:자연과함께, 4:유명한 관광지는 필수, 5:여유롭게 힐링, 6:문화－예술－역사, 7:여행지 느낌 물씬, 8:쇼핑은 열정적으로, 9:관광보다 먹방)")
@@ -49,6 +51,12 @@ public class ItineraryShowDTO {
         this.area = itinerary.getArea();
         this.startDate = itinerary.getStartDate();
         this.endDate = itinerary.getEndDate();
+
+        List<AccountShowDTO.Response> sharedList = new ArrayList<>();
+        for(ItineraryJoinAccount join : itinerary.getJoinAccount()){
+            Account joinAccount = join.getAccount();
+            sharedList.add(new AccountShowDTO.Response(joinAccount.getId(),joinAccount.getNickname(),joinAccount.getPhotoUrl(),joinAccount.getRole())); }
+        this.sharedAccount = sharedList;
 
         Account account = itinerary.getAccount();
         AccountShowDTO.Response accountDTO = new AccountShowDTO.Response(account.getId(),account.getNickname(),account.getPhotoUrl(),account.getRole());
