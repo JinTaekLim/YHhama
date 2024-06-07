70336dc445bbfbe3570cfa640ab4f6d3b59c42d5
JinTaekLim <jintaek019@gmail.com>
2024-06-08 00:45:48 +0900

    [추가] addPlace 장소 저장

---
+++ src/main/java/com/YH/yeohaenghama/domain/addPlace/repository/AddPlaceRepository.java
@@ -0,0 +1,7 @@
+package com.YH.yeohaenghama.domain.addPlace.repository;
+
+import com.YH.yeohaenghama.domain.addPlace.entity.AddPlace;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AddPlaceRepository extends JpaRepository<AddPlace, Long> {
+}
