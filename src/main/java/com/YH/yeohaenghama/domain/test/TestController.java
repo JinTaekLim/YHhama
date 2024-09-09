ca6b7c1db67e1cac8dde80f3b91743a0b55c61a1
JinTaekLim <jintaek019@gmail.com>
2024-09-09 14:44:27 +0900

    [추가] ChatAI read sql 변경, 예시 질문 리스트 등록

--- src/main/java/com/YH/yeohaenghama/domain/test/TestController.java
+++ src/main/java/com/YH/yeohaenghama/domain/test/TestController.java
@@ -1,19 +1,69 @@
-//package com.YH.yeohaenghama.domain.test;
-//
-//import io.swagger.v3.oas.annotations.Operation;
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//import java.util.Map;
-//
-//@Slf4j
-//@RequiredArgsConstructor
-//@RestController
-//@RequestMapping("/api/test")
-//public class TestController {
+package com.YH.yeohaenghama.domain.test;
+
+import com.YH.yeohaenghama.domain.chatAI.entity.ChatType;
+import com.YH.yeohaenghama.domain.chatAI.service.ChatAIService2;
+import com.YH.yeohaenghama.domain.chatAI.service.ChatAnswerService;
+import com.YH.yeohaenghama.domain.chatAI.service.ChatTypeService;
+import io.swagger.v3.oas.annotations.Operation;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/test")
+public class TestController {
+
+  private final ChatAIService2 chatAIService2;
+  private final ChatAnswerService chatAnswerService;
+  private final ChatTypeService chatTypeService;
+
+
+
+  @GetMapping("/read")
+  public void test() throws IOException {
+    Resource resource = new ClassPathResource("question.csv");
+    try (BufferedReader reader = new BufferedReader(new InputStreamReader(resource.getInputStream()));
+        CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT.withHeader())) {
+
+      for (CSVRecord record : csvParser) {
+        String question = record.get("question").trim();
+        String answer = record.get("answer").trim();
+        String type = record.get("type").trim();
+
+        System.out.println("Question: " + question);
+        System.out.println("Answer: " + answer);
+        System.out.println("Type: " + type);
+        System.out.println();
+
+        ChatType chatType = chatTypeService.getType(type);
+        String answerId = chatAnswerService.getAnswerId(answer, chatType);
+
+        chatAIService2.insertQuestion(question, answerId);
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
 //
 //    private final TestRepository testRepository;
 //    @Operation(summary = "저장")
@@ -42,4 +92,4 @@
 //    public void delete(Long accountId){
 //
 //    }
-//}
+}
